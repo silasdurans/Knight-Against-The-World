1a5135b9687bd7b897227084abe981ca8736b579
Silas Almeida Durans Lima <silasdurans@hotmail.com>
2025-03-08 10:54:41 -0300

    Finishing the game :tada:

--- First Game/Scripts/game_manager.gd
+++ First Game/Scripts/game_manager.gd
@@ -3,7 +3,16 @@ extends Node
 var Score = 0;
 
 @onready var score_label = $ScoreLabel
+@onready var allcoins = $"../Allcoins"
+
 
 func add_point():
 	Score += 1;
 	score_label.text = 'You collected ' + str(Score) + ' coins.';
+	
+	if Score >= 15:
+		allcoins.start();
+
+
+func _on_allcoins_timeout():
+	get_tree().change_scene_to_file("res://Scenes/finish.tscn");
